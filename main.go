a12273687ad2509ec070e9ca43faf997f0c5976d
ZmoT <nikitstri@gmail.com>
2025-02-23 18:16:05 +0300

    Использование переменных в Go; Импорт нескольких пакетов

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var userHeight = 1.9
+	var userKg float64 = 95
+	var BMI = userKg / math.Pow(userHeight, 2)
+	fmt.Println(BMI)
 }
