40a01a81e0872cedb382cee94e614130977476e3
ZmoT <nikitstri@gmail.com>
2025-02-23 19:40:01 +0300

    Добавлена возможность вводить и выводить информацию

--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 
 func main() {
 	const BMIPower = 2
-	var userHeight = 1.9
-	var userKg float64 = 95
-	var BMI = userKg / math.Pow(userHeight, BMIPower)
-	fmt.Println(BMI)
+	var userHeight float64 = 1.8
+	var userKg float64 = 100.0
+	fmt.Print("Калькулятор индекса массы тела ___ \n")
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userKg)
+	BMI := userKg / math.Pow(userHeight, BMIPower)
+	fmt.Print("Ваш индекс массы телы: ")
+	fmt.Print(BMI)
 }
