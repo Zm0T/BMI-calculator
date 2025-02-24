34e2120c59497dc18e835e1849f217a5e045cf67
ZmoT <nikitstri@gmail.com>
2025-02-24 19:29:47 +0300

    Добавлено использование доп функций

--- main.go
+++ main.go
@@ -9,12 +9,24 @@ func main() {
 	const BMIPower = 2
 	var userHeight float64 = 1.8
 	var userKg float64 = 100.0
-	fmt.Print("Калькулятор индекса массы тела ___ \n")
-	fmt.Print("Введите свой рост в метрах: ")
+
+	fmt.Println("___ Калькулятор индекса массы тела ___")
+	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
+
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
-	BMI := userKg / math.Pow(userHeight, BMIPower)
-	fmt.Print("Ваш индекс массы телы: ")
-	fmt.Print(BMI)
+
+	var IMT float64 = calculateBMI(userKg, userHeight, BMIPower)
+
+	outputResult(IMT)
+}
+
+func outputResult(BMI float64) {
+	fmt.Printf("Ваш индекс массы телы: %.0f", BMI)
+}
+
+func calculateBMI(userKg float64, userHeight float64, BMIPower float64) float64 {
+	BMI := userKg / math.Pow(userHeight/100, BMIPower)
+	return BMI
 }
